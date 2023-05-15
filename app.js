92f4160cb6ab93c6a67dd45bb43f52ad550e7d60
Harshvardhan Sinha <harshvardhansinha302@gmail.com>
2023-05-15 14:49:57 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const cluster = require("cluster");
+const os = require("os");
+const numCpus = 3;
+const handleRequest = require('./index');
+
+if (cluster.isMaster) {
+  console.log(`This is a Master Process with PID ${process.pid}`);
+  for (let i = 0; i < numCpus; i++) cluster.fork();
+} else {
+  console.log(`This is a worker process with PID ${process.pid}`);
+  const express = require("express");
+  const app = express();
+
+  app.use(handleRequest);
+
+  app.listen(3000 + cluster.worker.id, () => {
+    console.log(`Worker ${cluster.worker.id} has started`);
+  });
+}
---
+++ index.js
@@ -0,0 +1,43 @@
+const crypto = require("crypto");
+
+class HashRing {
+  constructor(nodes) {
+    this.nodes = nodes;
+    this.ring = [];
+    for (let node of this.nodes) {
+      for (let i = 0; i < 3; i++) {
+        const hash = crypto
+          .createHash("sha256")
+          .update(node + ":" + i)
+          .digest("hex");
+        const keyHash = parseInt(hash.substring(0, 8), 16);
+        this.ring.push([keyHash, node]);
+      }
+      this.ring.sort((a, b) => a[0] - b[0]);
+    }
+  }
+
+  getNode(key) {
+    const hash = crypto.createHash("sha256").update(key).digest("hex");
+    const keyHash = parseInt(hash.substr(0, 8), 16);
+    for (let i = 0; i < this.ring.length; i++) {
+      if (keyHash <= this.ring[i][0]) {
+        return this.ring[i][1];
+      }
+    }
+    return this.ring[0][1];
+  }
+}
+
+const nodes = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3002",
+];
+const ring = new HashRing(nodes);
+
+module.exports = handleRequest = (req, res) => {
+    console.log('Hellloooo');
+  let node = ring.getNode(req.url);
+  res.status(200).send(`Request ${req.url} is assigned to ${node}`);
+};
